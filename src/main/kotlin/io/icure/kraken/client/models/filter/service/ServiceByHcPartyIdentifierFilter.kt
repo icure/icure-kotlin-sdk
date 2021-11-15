0d383c1c74355f6326dd0621b50a97ca31c05ac6
Clément Vandendaelen <cv@taktik.com>
2021-11-15 13:48:45 +0100

    Added ServiceByHcPartyIdentifierFilter

---
+++ src/main/kotlin/io/icure/kraken/client/models/filter/service/ServiceByHcPartyIdentifierFilter.kt
@@ -0,0 +1,11 @@
+package io.icure.kraken.client.models.filter.service
+
+import io.icure.kraken.client.models.decrypted.ServiceDto
+import io.icure.kraken.client.models.filter.AbstractFilterDto
+
+data class ServiceByHcPartyIdentifierFilter(
+    val healthcarePartyId: String? = null,
+    val system: String? = null,
+    val value: String? = null,
+    override val desc: String? = null
+) : AbstractFilterDto<ServiceDto>

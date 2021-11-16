aff69e11471fb9e1d24fa059d35cb2901c52e504
Clément Vandendaelen <cv@taktik.com>
2021-11-16 10:54:40 +0100

    Added missing annotations

--- src/main/kotlin/io/icure/kraken/client/models/filter/service/ServiceByHcPartyIdentifierFilter.kt
+++ src/main/kotlin/io/icure/kraken/client/models/filter/service/ServiceByHcPartyIdentifierFilter.kt
@@ -1,8 +1,12 @@
 package io.icure.kraken.client.models.filter.service
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import io.icure.kraken.client.models.ServiceDto
 import io.icure.kraken.client.models.filter.AbstractFilterDto
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ServiceByHcPartyIdentifierFilter(
     val healthcarePartyId: String? = null,
     val system: String? = null,

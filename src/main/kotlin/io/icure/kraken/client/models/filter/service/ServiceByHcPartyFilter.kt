2f89bf330fcd68124c59363e283ea6a613ad753f
Antoine Duchâteau <ad@taktik.com>
2022-02-04 17:40:52 +0100

    Add filters

--- src/main/kotlin/io/icure/kraken/client/models/filter/service/ServiceByHcPartyFilter.kt
+++ src/main/kotlin/io/icure/kraken/client/models/filter/service/ServiceByHcPartyFilter.kt
@@ -19,11 +19,12 @@ package io.icure.kraken.client.models.filter.service
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
-import org.taktik.icure.services.external.rest.v1.dto.filter.AbstractFilterDto
+import io.icure.kraken.client.models.ServiceDto
+import io.icure.kraken.client.models.filter.AbstractFilterDto
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ServiceByHcPartyFilter(
-        override val hcpId: String,
+        val hcpId: String,
         override val desc:String? = null
 ) : AbstractFilterDto<ServiceDto>

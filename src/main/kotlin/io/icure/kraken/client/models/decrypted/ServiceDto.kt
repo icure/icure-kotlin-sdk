3f71fe13d2abd378f43a59286fa5adb67963db57
Clément Vandendaelen <cv@taktik.com>
2022-05-13 10:33:58 +0200

    Regenerated API

--- src/main/kotlin/io/icure/kraken/client/models/decrypted/ServiceDto.kt
+++ src/main/kotlin/io/icure/kraken/client/models/decrypted/ServiceDto.kt
@@ -228,7 +228,8 @@ data class ServiceDto (
         @JsonProperty("focus") focus("focus"),
         @JsonProperty("hasMember") hasMember("hasMember"),
         @JsonProperty("performer") performer("performer"),
-        @JsonProperty("specimen") specimen("specimen");
+        @JsonProperty("specimen") specimen("specimen"),
+        @JsonProperty("resultInterpreter") resultInterpreter("resultInterpreter");
     }
 }
 

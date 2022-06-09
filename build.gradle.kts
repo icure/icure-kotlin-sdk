93e13a17505a56b1888a99c5346d6fd571b01a4d
Clément Vandendaelen <cv@taktik.com>
2022-06-09 10:50:28 +0200

    Lower Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,14 +95,14 @@ dependencies {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "16"
     }
 }
 

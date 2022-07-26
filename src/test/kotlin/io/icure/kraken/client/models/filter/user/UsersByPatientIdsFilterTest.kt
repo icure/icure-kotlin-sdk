cee412f9d4f29e92e817c1a2fb0fb8621407d665
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-07-26 15:34:34 +0200

    Added UsersByPatientIdFilter

---
+++ src/test/kotlin/io/icure/kraken/client/models/filter/user/UsersByPatientIdsFilterTest.kt
@@ -0,0 +1,70 @@
+package io.icure.kraken.client.models.filter.user
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.SingletonSupport
+import io.icure.kraken.client.apis.UserApi
+import io.icure.kraken.client.infrastructure.UsernamePassword
+import io.icure.kraken.client.models.UserDto
+import io.icure.kraken.client.models.filter.chain.FilterChain
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import utils.removeEntities
+import java.util.*
+
+@OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
+class UsersByPatientIdsFilterTest : StringSpec ({
+
+    val createdEntitiesId = mutableListOf<String>()
+
+    afterSpec {
+        removeEntities(createdEntitiesId)
+    }
+
+    "The UsersByPatientIdsFilter is able to get all the users with a certain PatientId" {
+        val api = UserApi(basePath = System.getenv("TEST_ICURE_URL"),
+                            authHeader = UsernamePassword(System.getenv("ICURE_USR"), System.getenv("ICURE_PWD")).toBasicAuth())
+        val filterPatientId = UUID.randomUUID().toString()
+        val user1Uuid = UUID.randomUUID().toString()
+        val user1 = api.createUser(UserDto(
+            id = user1Uuid,
+            login = user1Uuid.substring(0, 8),
+            status = UserDto.Status.aCTIVE,
+            patientId = filterPatientId
+        ))
+        user1 shouldNotBe null
+        createdEntitiesId.add(user1Uuid)
+
+        val user2Uuid = UUID.randomUUID().toString()
+        val user2 = api.createUser(UserDto(
+            id = user2Uuid,
+            login = user2Uuid.substring(0, 8),
+            status = UserDto.Status.aCTIVE,
+            patientId = "PATIENT-2"
+        ))
+        user2 shouldNotBe null
+        createdEntitiesId.add(user2Uuid)
+
+        val user3Uuid = UUID.randomUUID().toString()
+        val user3 = api.createUser(UserDto(
+            id = user3Uuid,
+            login = user3Uuid.substring(0, 8),
+            status = UserDto.Status.aCTIVE,
+            patientId = filterPatientId
+        ))
+        user3 shouldNotBe null
+        createdEntitiesId.add(user3Uuid)
+
+        val filteredResult = api.filterUsersBy(FilterChain(UsersByPatientIdFilter(filterPatientId)), null, null)
+        filteredResult shouldNotBe null
+        filteredResult.rows shouldNotBe null
+        filteredResult.rows.size shouldBe 2
+        filteredResult.rows.forEach {
+            it.patientId shouldNotBe null
+            it.patientId shouldBe filterPatientId
+        }
+    }
+
+})

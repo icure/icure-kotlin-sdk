9cba1a098180aa8a8a9f7062a5563b3dbc386373
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-02-08 17:52:21 +0100

    ICBE-124 updated X Article API

--- src/main/kotlin/io/icure/kraken/client/extendedapis/ArticleApi.kt
+++ src/main/kotlin/io/icure/kraken/client/extendedapis/ArticleApi.kt
@@ -6,16 +6,17 @@ import io.icure.kraken.client.crypto.CryptoUtils.decryptAES
 import io.icure.kraken.client.crypto.CryptoUtils.encryptAES
 import io.icure.kraken.client.crypto.keyFromHexString
 import org.taktik.icure.services.external.rest.v2.dto.embed.DelegationDto
-import io.icure.kraken.client.models.UserDto
 import io.icure.kraken.client.models.decrypted.ArticleDto
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.taktik.icure.services.external.rest.v2.dto.UserDto
+import org.taktik.icure.services.external.rest.v2.dto.embed.DelegationTagDto
 import java.util.*
 
 suspend fun ArticleDto.initDelegations(
     user: UserDto,
-    config: CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>
+    config: CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>
 ): ArticleDto {
-    val delegations = (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf())
+    val delegations = (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf())
     val ek = UUID.randomUUID().toString()
     val sfk = UUID.randomUUID().toString()
     return this.copy(
@@ -24,20 +25,20 @@ suspend fun ArticleDto.initDelegations(
         delegations = (delegations + user.dataOwnerId()).fold(this.delegations) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     user.dataOwnerId(),
                     d,
                     config.crypto.encryptAESKeyForDataOwner(user.dataOwnerId(), d, this.id, sfk).first,
+                    emptyList()
                 ),
             ))
         },
         encryptionKeys = (delegations + user.dataOwnerId()).fold(this.encryptionKeys) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     user.dataOwnerId(),
                     d,
                     config.crypto.encryptAESKeyForDataOwner(user.dataOwnerId(), d, this.id, ek).first,
+                    emptyList()
                 ),
             ))
         },
@@ -49,12 +50,12 @@ suspend fun ArticleDto.initDelegations(
 suspend fun ArticleApi.createArticle(
     user: UserDto,
     article: ArticleDto,
-    config: CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>
+    config: CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>
 ) =
     this.createArticle(
         config.encryptArticle(
             user.dataOwnerId(),
-            (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf()),
+            (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf()),
             article
         )
     ).let { config.decryptArticle(user.dataOwnerId(), it) }
@@ -64,7 +65,7 @@ suspend fun ArticleApi.createArticle(
 suspend fun ArticleApi.getArticle(
     user: UserDto,
     articleId: String,
-    config: CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>
+    config: CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>
 ): ArticleDto {
     return this.getArticle(articleId).let { config.decryptArticle(user.dataOwnerId(), it) }
 }
@@ -73,7 +74,7 @@ suspend fun ArticleApi.getArticle(
 @ExperimentalStdlibApi
 suspend fun ArticleApi.getArticles(
     user: UserDto,
-    config: CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>
+    config: CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>
 ): List<ArticleDto> {
     return this.getArticles().map { config.decryptArticle(user.dataOwnerId(), it) }
 }
@@ -83,22 +84,22 @@ suspend fun ArticleApi.getArticles(
 suspend fun ArticleApi.modifyArticle(
     user: UserDto,
     article: ArticleDto,
-    config: CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>
+    config: CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>
 ): ArticleDto {
     return this.modifyArticle(
         config.encryptArticle(
             user.dataOwnerId(),
-            (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf()),
+            (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf()),
             article
         )
     ).let { config.decryptArticle(user.dataOwnerId(), it) }
 }
 
-suspend fun CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>.encryptArticle(
+suspend fun CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>.encryptArticle(
     myId: String,
     delegations: Set<String>,
     article: ArticleDto
-): io.icure.kraken.client.models.ArticleDto {
+): org.taktik.icure.services.external.rest.v2.dto.ArticleDto {
     return if (article.encryptionKeys.any { (_, s) -> s.isNotEmpty() }) {
         article
     } else {
@@ -106,10 +107,10 @@ suspend fun CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>.e
         article.copy(encryptionKeys = (delegations + myId).fold(article.encryptionKeys) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     myId,
                     d,
-                    this.crypto.encryptAESKeyForDataOwner(myId, d, article.id, secret).first
+                    this.crypto.encryptAESKeyForDataOwner(myId, d, article.id, secret).first,
+                    emptyList()
                 )
             ))
         })
@@ -125,9 +126,9 @@ suspend fun CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>.e
     }
 }
 
-suspend fun CryptoConfig<ArticleDto, io.icure.kraken.client.models.ArticleDto>.decryptArticle(
+suspend fun CryptoConfig<ArticleDto, org.taktik.icure.services.external.rest.v2.dto.ArticleDto>.decryptArticle(
     myId: String,
-    article: io.icure.kraken.client.models.ArticleDto
+    article: org.taktik.icure.services.external.rest.v2.dto.ArticleDto
 ): ArticleDto {
     val key = this.crypto.decryptEncryptionKeys(myId, article.encryptionKeys).firstOrNull()?.replace(
         "-",

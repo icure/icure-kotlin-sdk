7a4beef3ffa651b7f21258c9f512a967b9f8cc57
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-02-09 09:38:50 +0100

    ICBE-124 Updated X Receipt API

--- src/main/kotlin/io/icure/kraken/client/extendedapis/ReceiptApi.kt
+++ src/main/kotlin/io/icure/kraken/client/extendedapis/ReceiptApi.kt
@@ -5,18 +5,20 @@ import io.icure.kraken.client.crypto.CryptoConfig
 import io.icure.kraken.client.crypto.CryptoUtils.decryptAES
 import io.icure.kraken.client.crypto.CryptoUtils.encryptAES
 import io.icure.kraken.client.crypto.keyFromHexString
-import io.icure.kraken.client.models.*
 import io.icure.kraken.client.models.decrypted.ReceiptDto
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import org.taktik.icure.services.external.rest.v2.dto.UserDto
+import org.taktik.icure.services.external.rest.v2.dto.embed.DelegationDto
+import org.taktik.icure.services.external.rest.v2.dto.embed.DelegationTagDto
 import java.nio.ByteBuffer
 import java.util.*
 
 suspend fun ReceiptDto.initDelegations(
     user: UserDto,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ): ReceiptDto {
-    val delegations = (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf())
+    val delegations = (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf())
     val ek = UUID.randomUUID().toString()
     val sfk = UUID.randomUUID().toString()
     return this.copy(
@@ -25,20 +27,20 @@ suspend fun ReceiptDto.initDelegations(
         delegations = (delegations + user.dataOwnerId()).fold(this.delegations) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     user.dataOwnerId(),
                     d,
                     config.crypto.encryptAESKeyForDataOwner(user.dataOwnerId(), d, this.id, sfk).first,
+                    emptyList()
                 ),
             ))
         },
         encryptionKeys = (delegations + user.dataOwnerId()).fold(this.encryptionKeys) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     user.dataOwnerId(),
                     d,
                     config.crypto.encryptAESKeyForDataOwner(user.dataOwnerId(), d, this.id, ek).first,
+                    emptyList()
                 ),
             ))
         },
@@ -50,12 +52,12 @@ suspend fun ReceiptDto.initDelegations(
 suspend fun ReceiptApi.createReceipt(
     user: UserDto,
     receipt: ReceiptDto,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ) =
     this.createReceipt(
         config.encryptReceipt(
             user.dataOwnerId(),
-            (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf()),
+            (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf()),
             receipt
         )
     ).let { config.decryptReceipt(user.dataOwnerId(), it) }
@@ -65,7 +67,7 @@ suspend fun ReceiptApi.createReceipt(
 suspend fun ReceiptApi.getReceipt(
     user: UserDto,
     receiptId: String,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ): ReceiptDto {
     return this.getReceipt(receiptId).let { config.decryptReceipt(user.dataOwnerId(), it) }
 }
@@ -75,12 +77,12 @@ suspend fun ReceiptApi.getReceipt(
 suspend fun ReceiptApi.modifyReceipt(
     user: UserDto,
     receipt: ReceiptDto,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ): ReceiptDto {
     return this.modifyReceipt(
         config.encryptReceipt(
             user.dataOwnerId(),
-            (user.autoDelegations["all"] ?: setOf()) + (user.autoDelegations["medicalInformation"] ?: setOf()),
+            (user.autoDelegations[DelegationTagDto.all] ?: setOf()) + (user.autoDelegations[DelegationTagDto.medicalInformation] ?: setOf()),
             receipt
         )
     ).let { config.decryptReceipt(user.dataOwnerId(), it) }
@@ -91,7 +93,7 @@ suspend fun ReceiptApi.modifyReceipt(
 suspend fun ReceiptApi.listByReference(
     user: UserDto,
     ref: String,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ): List<ReceiptDto> {
     return this.listByReference(ref).map { config.decryptReceipt(user.dataOwnerId(), it) }
 }
@@ -104,17 +106,17 @@ suspend fun ReceiptApi.setReceiptAttachment(
     blobType: String,
     requestBody: Flow<ByteBuffer>,
     enckeys: String?,
-    config: CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>
+    config: CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>
 ): ReceiptDto {
     return this.setReceiptAttachment(receiptId, blobType, requestBody, enckeys)
         .let { config.decryptReceipt(user.dataOwnerId(), it) }
 }
 
-suspend fun CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>.encryptReceipt(
+suspend fun CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>.encryptReceipt(
     myId: String,
     delegations: Set<String>,
     receipt: ReceiptDto
-): io.icure.kraken.client.models.ReceiptDto {
+): org.taktik.icure.services.external.rest.v2.dto.ReceiptDto {
     return if (receipt.encryptionKeys.any { (_, s) -> s.isNotEmpty() }) {
         receipt
     } else {
@@ -122,10 +124,10 @@ suspend fun CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>.e
         receipt.copy(encryptionKeys = (delegations + myId).fold(receipt.encryptionKeys) { m, d ->
             m + (d to setOf(
                 DelegationDto(
-                    emptyList(),
                     myId,
                     d,
-                    this.crypto.encryptAESKeyForDataOwner(myId, d, receipt.id, secret).first
+                    this.crypto.encryptAESKeyForDataOwner(myId, d, receipt.id, secret).first,
+                    emptyList()
                 )
             ))
         })
@@ -141,9 +143,9 @@ suspend fun CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>.e
     }
 }
 
-suspend fun CryptoConfig<ReceiptDto, io.icure.kraken.client.models.ReceiptDto>.decryptReceipt(
+suspend fun CryptoConfig<ReceiptDto, org.taktik.icure.services.external.rest.v2.dto.ReceiptDto>.decryptReceipt(
     myId: String,
-    receipt: io.icure.kraken.client.models.ReceiptDto
+    receipt: org.taktik.icure.services.external.rest.v2.dto.ReceiptDto
 ): ReceiptDto {
     val key = this.crypto.decryptEncryptionKeys(myId, receipt.encryptionKeys).firstOrNull()?.replace(
         "-",

cc770cb9d51a5424490f6ab06ac75ab571858748
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-02-28 15:40:59 +0100

    Bumped DTO version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val kotlinVersion = "1.8.0"
 val kotlinCoroutinesVersion = "1.6.2"
 val jacksonVersion = "2.12.5"
 val kmapVersion = "0.1.52-main.8d4a565b58"
-val krakenLibsVersion = "4.0.73-gce284629a6"
+val krakenLibsVersion = "4.0.76-g7362d7284b"
 
 plugins {
     kotlin("jvm") version "1.8.0"

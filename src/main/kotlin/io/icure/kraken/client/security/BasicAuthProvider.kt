671c47af081426950324bc381b84f4f7cb6eae33
Clément VANDENDAELEN <cv@icure.com>
2023-05-22 14:28:05 +0200

    BasicAuthProvider now allows to directly pass the basic token

--- src/main/kotlin/io/icure/kraken/client/security/BasicAuthProvider.kt
+++ src/main/kotlin/io/icure/kraken/client/security/BasicAuthProvider.kt
@@ -1,9 +1,21 @@
 package io.icure.kraken.client.security
 
 class BasicAuthProvider(
-    private val username: String,
-    val password: String
+    private val username: String?,
+    private val password: String?,
+    private val basicToken: String?
 ) : AuthProvider {
-    override suspend fun getAuthHeader(): String =
-        "Basic ${java.util.Base64.getEncoder().encodeToString("$username:$password".toByteArray())}"
+    override suspend fun getAuthHeader(): String = when {
+        !username.isNullOrEmpty() && !password.isNullOrEmpty() -> getAuthString(username, password)
+        !basicToken.isNullOrEmpty() -> when (basicToken.contains(":")) {
+            true -> getAuthString(basicToken.substringBefore(":"), basicToken.substringAfter(":"))
+            false -> "Basic $basicToken"
+        }
+        else -> throw IllegalArgumentException("BasicAuthProvider: username/password or basicToken must be provided")
+    }
+
+    companion object {
+        private fun getAuthString(username: String, password: String): String =
+            "Basic ${java.util.Base64.getEncoder().encodeToString("$username:$password".toByteArray())}"
+    }
 }
